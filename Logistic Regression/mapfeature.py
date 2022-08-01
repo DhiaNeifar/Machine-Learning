267556feb7a7f111350eb6c342acd46be667f46b
dhianeifar <dhia.neifar@supcom.tn>
2022-08-02 00:56:16 +0100

    Finished the Decision Boundary Plot

--- Logistic Regression/mapfeature.py
+++ Logistic Regression/mapfeature.py
@@ -2,7 +2,7 @@ import pandas
 import pandas as pd
 
 
-def map_feature(df, n) -> pandas.DataFrame:
+def map_feature(df, n=6) -> pandas.DataFrame:
     cols = df.columns.tolist()
     col_1 = df[cols[0]]
     col_2 = df[cols[1]]
@@ -14,6 +14,17 @@ def map_feature(df, n) -> pandas.DataFrame:
         for j in range(i + 1):
             new_df[f'({i - j}, {j})'] = col_1.pow(i - j) * col_2.pow(j)
     return new_df
+
+
+def map_feature_row(fl1, fl2, theta, n=6) -> float:
+    comp, ind = 0, 0
+
+    for i in range(n + 1):
+        for j in range(i + 1):
+            print(f'theta {ind}, x1 {i - j}, x2 {j}')
+            comp += pow(fl1, i - j) * pow(fl2, j) * theta[ind]
+            ind += 1
+    return comp
 
 
 if __name__ == '__main__':
